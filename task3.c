a9ef3808c39046a66a6efc3e9c160e077def6563
oskarsan <oskar.sankila@gmail.com>
2024-12-16 13:54:02 +0200

    task 3

---
+++ task3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h> 
+
+
+
+int main(int argc, char *argv[]) {
+  
+
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        printf("Fork failed\n");
+        exit(1);
+    } else if (pid == 0) {
+        execvp(argv[1], &argv[1]);
+        perror("execvp");
+        exit(1);
+    } else {
+        int status;
+        waitpid(pid, &status, 0);
+        printf("Child process %d exited with status %d\n", pid, status);
+    }
+
+
+    return 0;
+}
