9b4f447635e801d48faeb0163a8cf004d58b46db
oskarsan <oskar.sankila@gmail.com>
2024-12-16 12:45:50 +0200

    task 2

--- task2.c
+++ task2.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h> 
+#include <string.h>
 
-int main() {
-    // Your code here
+int main(int argc, char *argv[]) {
+     
+    char command[256] = {0};
+    for (int i = 1; i < argc; i++) {
+        strcat(command, argv[i]);
+        strcat(command, " ");
+    }
+
+    printf("system():\n");
+    system(command);
+
+
+    printf("\nexecvp():\n");
+    execvp(argv[1], &argv[1]);
+
+    printf("process doesnt continue");
 
     return 0;
 }
